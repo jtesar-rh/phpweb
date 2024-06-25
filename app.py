28dafe5e18ec9e041026fbcec1cc472f22af6fa7
Jirka Tesar <jtesar@redhat.com>
2024-06-25 16:04:18 -0400

    lint

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 # Python 3 server example
+"""Module doc
+"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-
-hostName = "0.0.0.0"
-serverPort = 8080
-
+HOST_NAME = "0.0.0.0"
+SERVER_PORT = 8080
 class MyServer(BaseHTTPRequestHandler):
+    """Class docs
+    """
     def do_GET(self):
+        """Calculates the area of a rectangle.
+
+          This function takes the length and width of a rectangle as arguments
+          and returns the calculated area.
+
+          Args:
+              length: The length of the rectangle (float).
+              width: The width of the rectangle (float).
+
+          Returns:
+              The area of the rectangle (float).
+          """
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an updated nr 4 example web server.....</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("Hello World!", "utf-8"))
 
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+if __name__ == "__main__":
+    webServer = HTTPServer((HOST_NAME, SERVER_PORT), MyServer)
 
     try:
         webServer.serve_forever()
